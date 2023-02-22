0eabf20f5617b07081e6dc50e6f4119adeb0c767
Eparrr <113688837+Eparrr@users.noreply.github.com>
2023-02-22 18:26:38 +0300

    Add files via upload

---
+++ week_2/B.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+#ifndef N
+#define N 5
+#endif
+
+void shiftRight(int (&arr)[N]){
+    for (int i=N-1;i>0;i--){
+        swap(arr[i], arr[i-1]);
+
+    }
+}
+
+int main(){
+    int arr[N];
+    for (int i=0;i<N;i++){
+        cin >> arr[i];
+    }
+    shiftRight(arr);
+     for (int i=0;i<N;i++){
+        cout << arr[i];
+    }
+}
